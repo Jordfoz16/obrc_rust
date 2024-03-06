d6c6e88a2edafac48a73aac6c736a4b7d19728cb
Jordan <jordfoz@gmail.com>
2024-03-06 09:37:37 +0000

    6m 11s run

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
 use std::collections::{BTreeMap, HashMap};
-use std::fs;
 use std::fs::File;
 use std::io::{Lines, BufRead, BufReader};
 use std::time::Instant;
-
-
-struct Measurement {
-    city: String,
-    temp: f32
-}
 
 struct Results {
     min: f32,
@@ -32,49 +25,39 @@ fn read_file() -> Lines<BufReader<File>>{
     return lines;
 }
 
-
-fn parse_line(line: &str) -> Measurement {
-    //Saint Petersburg;7.5
-    let mut split_line = line.split(";");
-
-    let city = split_line.next().unwrap().to_string();
-    let temp: f32 = split_line.next().unwrap().parse().unwrap();
-
-    let measurement = Measurement{city: city, temp: temp};
-
-    return measurement;
-}
-
-
-fn calculate_results(measurements: Vec<Measurement>) -> HashMap<String, Results>{
-    
+fn calculate(lines: Lines<BufReader<File>>) -> HashMap<String, Results> {
     let mut city_map: HashMap<String, Results> = HashMap::new();
+    
+    for line in lines {
+        let test = line.unwrap();
+        let mut split_line = test.split(";");
 
-    for m in measurements {
-        
-        let current_measurement = city_map.entry(m.city.clone()).or_insert(Results {
+        let city = split_line.next().unwrap().to_string();
+        let temp: f32 = split_line.next().unwrap().parse().unwrap();
+
+        let current_measurement = city_map.entry(city.clone()).or_insert(Results {
             min: f32::INFINITY,
             max: f32::NEG_INFINITY,
             sum: 0.0,
             count: 0,
         });
-        
-        if m.temp < current_measurement.min {
-            current_measurement.min = m.temp;
+
+        if temp < current_measurement.min {
+            current_measurement.min = temp;
         }
 
-        if m.temp > current_measurement.max {
-            current_measurement.max = m.temp;
+        if temp > current_measurement.max {
+            current_measurement.max = temp;
         }
 
-        current_measurement.sum += m.temp;
+        current_measurement.sum += temp;
         current_measurement.count += 1;
-        
     }
 
     return city_map;
     
 }
+
 
 fn print_results(results: BTreeMap<String, Results>){
 
@@ -99,8 +82,8 @@ fn print_results(results: BTreeMap<String, Results>){
 
     result += "}";
 
-    fs::write("result.txt", &result).expect("Error writing to file");
-    //println!("{}", result);
+    //fs::write("result.txt", &result).expect("Error writing to file");
+    println!("{}", result);
 
 }
 
@@ -108,22 +91,14 @@ fn main() {
     
     let start_time = Instant::now();
 
-    let lines = read_file();
+    let lines: Lines<BufReader<File>> = read_file();
 
-    let mut measurements: Vec<Measurement> = vec![];
-
-    for line in lines {
-        let t = line.unwrap().clone();
-        measurements.push(parse_line(&t));
-    }
-
-    let results: HashMap<String, Results> = calculate_results(measurements);
+    let results: HashMap<String, Results> = calculate(lines);
     let sorted_results: BTreeMap<String, Results> = results.into_iter().collect();
-
+    
     print_results(sorted_results);
 
     let end_time = Instant::now();
     let elapsed_time = end_time.duration_since(start_time);
-
     println!("Elapsed time: {:?}", elapsed_time);
 }
