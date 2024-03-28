85b4b891f0bd63ed10584bae9d4b0ee5f179aad3
Jordan <jordfoz@gmail.com>
2024-03-28 12:49:10 +0000

    before trying to make it faster

--- src/bin/single_thread.rs
+++ src/bin/single_thread.rs
@@ -1,6 +1,6 @@
 use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
-use std::io::{Lines, BufRead, BufReader, Read, Seek, SeekFrom};
+use std::io::{BufRead, BufReader, Lines, Read, Seek, SeekFrom, Write};
 use std::ptr::read;
 use std::time::Instant;
 use log::{error, info, Level};
@@ -13,15 +13,27 @@ struct Results {
 }
 
 #[derive(Clone)]
-struct Chunk {
+struct Chunk<const N: usize>{
     id: usize,
-    chunk: Vec<u8>
+    chunk: [u8; N],
+}
+
+impl Chunk<CHUNK_SIZE_KB> {
+    fn new(id: usize, data: &[u8]) -> Option<Self> {
+        if data.len() <= CHUNK_SIZE_KB {
+            let mut array = [0u8; CHUNK_SIZE_KB];
+            array[0..data.len()].copy_from_slice(&data);
+            Some(Self { id: id, chunk: array })
+        } else {
+            None
+        }
+    }
 }
 
 const BUFFER_CAPACITY: usize =  8000;
 
 const READ_BUFFER_SIZE_KB: usize = 16;
-const CHUNK_SIZE_KB: usize = 1024;
+const CHUNK_SIZE_KB: usize = 1 * 1024;
 
 fn read_file(path: &str) -> HashMap<String, Results>{
 
@@ -29,7 +41,7 @@ fn read_file(path: &str) -> HashMap<String, Results>{
 
     let file: File = File::open(path).expect("Error");
     let mut reader: BufReader<File> = BufReader::with_capacity(READ_BUFFER_SIZE_KB * 1024, file);
-    let mut buffer = [0; CHUNK_SIZE_KB * 1024];
+    let mut buffer = [0; CHUNK_SIZE_KB];
     let mut count = 0;
 
     loop {
@@ -61,12 +73,15 @@ fn read_file(path: &str) -> HashMap<String, Results>{
 
                 // let _ = tx.send_blocking(Chunk { id: count, chunk: chunk.to_vec() });
                 // info!("Queue Length {}", tx.len());
-                process_chunk(Chunk { id: count, chunk: chunk.to_vec() }, &mut results);
+                //process_chunk3(Chunk { id: count, chunk: chunk }, &mut results);
+                
+                process_chunk3(Chunk::new(count, chunk).unwrap(), &mut results);
             },
             None => {
                 // let _ = tx.send_blocking(Chunk { id: count, chunk: buffer[..bytes_read].to_vec() });
                 // info!("Queue Length {}", tx.len());
-                process_chunk(Chunk { id: count, chunk: buffer[..bytes_read].to_vec() }, &mut results);
+                //process_chunk3(Chunk { id: count, chunk: buffer[..bytes_read] }, &mut results);
+                process_chunk3(Chunk::new(count, buffer[..bytes_read].as_ref()).unwrap(), &mut results);
             }
         }
 
@@ -77,7 +92,7 @@ fn read_file(path: &str) -> HashMap<String, Results>{
     return results;
 }
 
-fn process_chunk(rx: Chunk,  result_tx: &mut HashMap<String, Results>){
+fn process_chunk(rx: Chunk<CHUNK_SIZE_KB>,  result_tx: &mut HashMap<String, Results>){
     
     let lines = rx.chunk.lines();
     let mut city_map = result_tx;
@@ -107,6 +122,101 @@ fn process_chunk(rx: Chunk,  result_tx: &mut HashMap<String, Results>){
         current_measurement.sum += temp;
         current_measurement.count += 1;
 
+    }
+}
+
+
+fn process_chunk2(rx: Chunk<CHUNK_SIZE_KB>,  result_tx: &mut HashMap<String, Results>){
+
+    //Scan each char to find new line
+
+    let mut temp_line: Vec<char> = vec!();
+    let mut city_map = result_tx;
+
+    for i in rx.chunk {
+
+        if i != 10 {
+            let character = match std::char::from_u32(i as u32) {
+                Some(c) => c,
+                None => panic!("Invalid UTF-8 integer!"),
+            };
+            temp_line.push(character);
+            continue;
+        }
+
+        let string_from_vec: String = temp_line.clone().into_iter().collect();
+
+        let mut split_line = string_from_vec.split(";");
+
+        let city = split_line.next().unwrap().to_string();
+        let temp: f32 = split_line.next().unwrap().parse().unwrap();
+
+        let current_measurement = city_map.entry(city.clone()).or_insert(Results {
+            min: f32::INFINITY,
+            max: f32::NEG_INFINITY,
+            sum: 0.0,
+            count: 0,
+        });
+
+        if temp < current_measurement.min {
+            current_measurement.min = temp;
+        }
+
+        if temp > current_measurement.max {
+            current_measurement.max = temp;
+        }
+
+        current_measurement.sum += temp;
+        current_measurement.count += 1;
+        
+        temp_line.clear();
+
+        
+    }
+}
+
+fn process_chunk3(rx: Chunk<CHUNK_SIZE_KB>,  result_tx: &mut HashMap<String, Results>){
+
+
+    let mut city_map = result_tx;
+    let mut split_test = rx.chunk.split(|num| num == &10)
+    .map(|a| {
+        let binding = String::from_utf8(a.to_vec()).unwrap();
+        let mut split_line = binding.split(|c| c == ';');
+
+        // let city = split_line.next().unwrap().to_string();
+        // let temp: f32 = split_line.next().unwrap().parse().unwrap();
+
+        let city = match split_line.next() {
+            Some(s) => s.to_string(),
+            None => return
+        };
+
+        let temp: f32 = match split_line.next() {
+            Some(s) => s.parse().unwrap(),
+            None => return
+        };
+
+        let current_measurement = city_map.entry(city.clone()).or_insert(Results {
+            min: f32::INFINITY,
+            max: f32::NEG_INFINITY,
+            sum: 0.0,
+            count: 0,
+        });
+
+        if temp < current_measurement.min {
+            current_measurement.min = temp;
+        }else if temp > current_measurement.max {
+            current_measurement.max = temp;
+        }
+
+        current_measurement.sum += temp;
+        current_measurement.count += 1;
+
+    });
+
+    for i in split_test {
+        //println!("{}", i.unwrap());
     }
 }
 
@@ -144,7 +254,7 @@ fn main() {
     let start_time = Instant::now();
 
     //let lines: Lines<BufReader<File>> = read_file();
-    let path = "data/measurements_10m.txt";
+    let path = "data/measurements_1m.txt";
     let results = read_file(path);
 
     //let results: HashMap<String, Results> = calculate(lines);
